d0218b288d4bf13c94da40d00cd76bbd539c5557
sriker99 <vootukuri.s@northeastern.edu>
2022-03-03 20:59:21 -0500

    last commit

--- public/tuiter/HomeScreen/ExploreScreen.js
+++ public/tuiter/HomeScreen/ExploreScreen.js
@@ -4,10 +4,10 @@ import postList from "./PostList.js";
 (function ($) {
     $('#wd-explore').append(`
         <div class="row">
-            <div class="col-xxl-2 col-xl-2 col-lg-1 col-md-3 col-sm-3">
+            <div class="col-xxl-2 col-xl-2 col-lg-1 col-md-2 col-sm-2">
                 ${NavigationSidebar('home')}
             </div>
-            <div class="col-xxl-6 col-xl-6 col-lg-7 col-md-11 col-sm-11">
+            <div class="col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-10">
                 ${postList()}
             </div>
             <div class="col-xxl-4 col-xl-4 col-lg-4 d-none d-lg-block">
